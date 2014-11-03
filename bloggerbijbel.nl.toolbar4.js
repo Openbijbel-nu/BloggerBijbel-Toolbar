6c777462c5adccde25a6b6f8ab7e84291683f8b3
OpenBijbel.nu Ontwikkelaar <Openbijbel.nu@gmail.com>
2014-11-03 18:12:19 +0100

    Update bloggerbijbel.nl.toolbar4.js

--- bloggerbijbel.nl.toolbar4.js
+++ bloggerbijbel.nl.toolbar4.js
@@ -123,6 +123,55 @@
 		});
  	}
 
+ 	/**
+ 	 * Adds a Biblia embedment in the extra column
+ 	 */
+	function embedBiblia() {
+		var startVerse = $("sup").first().text();
+		$(".OpenBijbelEmbeddedBiblia").html('<biblia:bible layout="minimal" resource="niv2011" width="400px" height="1200px" startingReference="' + startVerse + '"></biblia:bible>');
+		
+		var url = "//biblia.com/api/logos.biblia.js";
+		$.getScript( url, function() {
+			logos.biblia.init();
+		});
+	}
+	
+ 	/**
+ 	 * Split columns
+ 	 */
+ 	function splitColumns(extraColumnCount) {
+ 		$(".tr-1").after(
+ 			'<div class="openbijbelvertalingtekst">'
+ 				+ '<div id="OpenBijbelEmbeddedBiblia" class="OpenBijbelEmbeddedBiblia">'
+				+ '</div>'
+ 			+ '</div>'
+ 		);
+
+
+		embedBiblia();
+
+		$('.openbijbelvertaling').text(openBijbelToolBar.find(".openbijbelvertalingnaam").text());
+
+		$(".openbijbelvertalingtekst").css({
+			"float": "right",
+			"width": "40%",
+			"height": "100%",
+			"padding": "10px"
+		});
+
+		// breedte van translation - 30 voor bij 2 kolommen en 65 bij 1
+		if (extraColumnCount == 1) {
+			$(".translation").css({
+				"width": "58%"
+			});
+		} else if (extraColumnCount == 2) {
+			$(".translation").css({
+				"width": "29%"
+			});
+		}
+		
+ 	}
+
  	// This variable will be used to attach a jQuery reference to the Open Bijbel top bar. 
  	// So we can use it in multiple functions.
  	var openBijbelToolBar = undefined;
@@ -132,26 +181,28 @@
  	 */
  	function setupTopBar() {
  		// add the basics to the stickynotes top bar
- 		$(".navbar").prepend("<div class='openbijbeltoolbar'></div>");
+ 		$(".stickytop").prepend("<div class='openbijbeltoolbar'></div>");
 
- 		openBijbelToolBar = $(".openbijbeltoolbar");
+ 		openBijbelToolBar = $(".stickytop .openbijbeltoolbar");
 
  		// build the basic content of the toolbar
  		var toolbarContent = 
 			'<div class="openbijbelvertalingnaam openbijbelvertaling">[[|]] &nbsp; NIV</div>'
 			+ '<div class="openbijbelknoppenarea">'
-				+ '<span class="openbijbelknoptoelichting">Andere vertaling: </span>'
-				+ '<span class="openbijbelknop vertalingkeus NIV" data-translation="NIV">NIV </span>'
-				+ '<span class="openbijbelknop vertalingkeus ESV" data-translation="ESV">ESV </span>'
-				+ '<span class="openbijbelknop vertalingkeus KJV" data-translation="KJV">KJV </span>'
-				+ '<span class="openbijbelknop vertalingkeus NKJV" data-translation="NKJV">NKJV </span>'
-				+ '<span class="openbijbelknop vertalingkeus NLT" data-translation="NLT">NLT </span>'
+				+ '<span class="openbijbelknoptoelichting">Tooltip vertaling: </span>'
+				+ '<span class="openbijbelknop vertalingkeus NIV" data-translation="NIV">NIV</span> '
+				+ '<span class="openbijbelknop vertalingkeus ESV" data-translation="ESV">ESV</span> '
+				+ '<span class="openbijbelknop vertalingkeus KJV" data-translation="KJV">KJV</span> '
+				+ '<span class="openbijbelknop vertalingkeus NKJV" data-translation="NKJV">NKJV</span> '
+				+ '<span class="openbijbelknop vertalingkeus NLT" data-translation="NLT">NLT</span>'
+				+ '&nbsp; | &nbsp;'
+				+ '<span class="openbijbelknoptoelichting"> [[|]] Kolom: </span>'
+				+ '<span class="openbijbelknop weergavekeus kiesbibliakolom">Toevoegen</span>'
+				+ '<span class="openbijbelknop weergavekeus kiesreftagtooltip">Verwijderen</span>'
+				+ ' <span class="openbijbelknop kiesReset">(Reset)</span> '
 			+ '</div>';
 
  		openBijbelToolBar.append(toolbarContent);
-
-		openBijbelToolBar.find(".openbijbelknoppenarea").append(' <span class="openbijbelknop kiesReset">(Reset)</span> ');
-
 
  		// set styling
  		openBijbelToolBar.css({
@@ -203,11 +254,38 @@
  			chooseTranslation(translation);
  		});
 
- 		openBijbelToolBar.on('click', '.kiesReset', function() {
+ 		 		openBijbelToolBar.on('click', '.kiesReset', function() {
  			showReferences();
 
  			// choose default translation
  			chooseTranslation("NIV");
+ 			
+		});
+
+ 		openBijbelToolBar.on('click', '.kiesbibliakolom', function() {
+ 			splitColumns($('.translation').length);
+			$('.row').css("margin-left","0px");
+			$(this).hide();
+			openBijbelToolBar.find('.kiesreftagtooltip').show();
+			
+		});
+
+ 		openBijbelToolBar.on('click', '.kiesreftagtooltip', function() {
+ 			// doet niks
+			$('.openbijbelvertalingtekst').remove();
+			
+			// breedte teruggeven op basis van aantal aanwezige kolommen
+
+                        var columnCount = $('.translation').length;
+
+			if (columnCount > 1) {
+			     $(".translation").css("width","45%");
+			} else {
+			     $(".translation").css("width","90%");	
+			}
+
+			$(this).hide();
+			openBijbelToolBar.find('.kiesbibliakolom').show();
 		});
 
  	}
